2b209b0352e7ee4cc22242614ecf4a96aa58c6fa
charvey <charvey@securelink.com>
2019-02-07 10:44:28 -0600

    Anonymized information and added placeholder comments

--- NewUser.ps1
+++ NewUser.ps1
@@ -1,6 +1,6 @@
 Import-Module ActiveDirectory
 
-$Identity = Get-ADUser standarduser
+
 Write-Host "What is the user's first name?" -ForegroundColor Green
 $Givenname = Read-Host  
 Write-Host "What is the user's last name?" -ForegroundColor Green
@@ -10,7 +10,14 @@ $User = Read-Host
 Write-Host "Please enter the user password for this user" -ForegroundColor Green
 $Password = Read-Host -AsSecureString
 
-$Email = "$User@securelink.com"
-$OU = "OU=Corporate Accounts,OU=Users,OU=SecureLink,DC=sl,DC=lan"
+$Email = "$User@domain.com" #Enter your domain for email address
+$OU = "Path" #Enter your AD path for your organization
 
-New-ADUser -SamAccountName $User -AccountPassword $Password -UserPrincipalName "$User@sl.lan" -GivenName $Givenname -Surname $Surname -EmailAddress $Email -Enabled $true -ChangePasswordAtLogon $true -DisplayName "$Givenname $Surname" -Name "$Givenname $Surname" -Path $OU
+New-ADUser -SamAccountName $User `
+-AccountPassword $Password `
+-UserPrincipalName "$User@sl.lan" `
+-GivenName $Givenname -Surname $Surname `
+-EmailAddress $Email -Enabled $true `
+-ChangePasswordAtLogon $true `
+-DisplayName "$Givenname $Surname" -Name "$Givenname $Surname" `
+-Path $OU
--- SetPrimaryGroup.ps1
+++ SetPrimaryGroup.ps1
@@ -1,4 +1,4 @@
-$group = get-adgroup "SambaUsers"
+$group = get-adgroup "UserGroup" #Enter your Group name here
 $groupSid = $group.sid
 $groupSid
 [int]$GroupID = $groupSid.Value.Substring($groupSid.Value.LastIndexOf("-")+1)
