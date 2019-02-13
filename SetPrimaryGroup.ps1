5115b4e88b6be977127bd440fb9557c6784cb6d2
charvey <charvey@securelink.com>
2019-02-13 13:26:22 -0600

    Changed Set-ADObject to Set-ADUser in SetPrimaryGroup.ps1

--- SetPrimaryGroup.ps1
+++ SetPrimaryGroup.ps1
@@ -1,6 +1,9 @@
-$group = get-adgroup "UserGroup" #Enter your Group name here
+$group = get-adgroup "SambaUsers"
 $groupSid = $group.sid
 $groupSid
 [int]$GroupID = $groupSid.Value.Substring($groupSid.Value.LastIndexOf("-")+1)
 
-Get-ADUser "jsmith" | Set-ADObject -Replace @{primaryGroupID="$GroupID"}
+$Users = @("bjohnson", "jsmith") #Try to determine a way to get newly created users put into an array like this
+foreach ($User in $Users) { Set-ADUser $User -Replace @{primaryGroupID="$GroupID"} }
+
+# Get-ADUser "jsmith" | Set-ADObject -Replace @{primaryGroupID="$GroupID"}
