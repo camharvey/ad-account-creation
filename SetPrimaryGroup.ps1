bff01e84dcbdc74cad4afd767cac7f3e16329d43
charvey <charvey@securelink.com>
2019-06-26 13:46:36 -0500

    Incorporated all files to one single .ps1 file, CSV-ImportADUsers

--- SetPrimaryGroup.ps1
+++
@@ -1,9 +0,0 @@
-$group = get-adgroup "SambaUsers"
-$groupSid = $group.sid
-$groupSid
-[int]$GroupID = $groupSid.Value.Substring($groupSid.Value.LastIndexOf("-")+1)
-
-$Users = @("bjohnson", "jsmith") #Try to determine a way to get newly created users put into an array like this
-foreach ($User in $Users) { Set-ADUser $User -Replace @{primaryGroupID="$GroupID"} }
-
-# Get-ADUser "jsmith" | Set-ADObject -Replace @{primaryGroupID="$GroupID"}
